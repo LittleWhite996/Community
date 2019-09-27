a47eb13a152c90c44a346015650bb07b54535adf
TY <you@example.com>
2019-09-27 23:22:05 +0800

    first

---
+++ src/main/java/com/ty/expei/ExpeiApplication.java
@@ -0,0 +1,13 @@
+package com.ty.expei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpeiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExpeiApplication.class, args);
+    }
+
+}
